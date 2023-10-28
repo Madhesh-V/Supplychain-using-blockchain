c59d9d4a1feb82ba475cc85afd96dd4fd5f4b284
Madhesh V <83573352+Madhesh-V@users.noreply.github.com>
2023-10-28 13:12:44 +0530

    Delete public/src directory

--- public/src/mintNFT.sol
+++
@@ -1,5 +0,0 @@
-function mintNFT(address to, string memory tokenURI) public onlyOwner {
-    uint256 tokenId = totalSupply().add(1);
-    _mint(to, tokenId);
-    _setTokenURI(tokenId, tokenURI);
-}
